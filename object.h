3b9d2e6c045b98dada659abe87297b1d13a9d145
Coosome <thecoltonjelsema@gmail.com>
2020-11-10 17:57:40 -0800

    Name move and formatting
    >"Name" moved, since not all derived objects will require names (obsticle, treasure)
    
    >Rearanged some variables to be as consistant as possible
    
    >Modified some spacing for consistency

--- object.h
+++ object.h
@@ -27,18 +27,13 @@ class object
 	public:
 	object(); //default constructor
 	object(const object & to_copy); //copy constructor
-	object(char *add_name, int add_amount); //constructor with args
+	object(char *add_type, int add_cost); //constructor with args
 	virtual ~object();//destructor
 	virtual void display(); //display for the base clas
 
-
-
 	protected:
-	char *name;
-	char *type;
+	char *type; //may want to be changed to an intiger, given only 4 types
 	int cost;
-
-
 
 };
 
@@ -47,15 +42,16 @@ class food: public object
 {
 	public:
 	food(); //default constructor
-	food(char *add_name, int add_cost, char * add_type, int value); //constuctor with args
+	food(char *add_type, int add_cost, char *add_name, int value); //constuctor with args
 	food(const food &to_copy); //copy constructor
 	~food(); //destructor
 	void display();
 
 	protected:
+	char *name;
 	int value;
-};
 
+};
 
 
 class tool: public object
@@ -63,47 +59,48 @@ class tool: public object
 	public:
 	tool(); //default constructor
 	//constructor with args
-	tool(char *add_name, int add_cost, char *add_type, int add_power); 
+	tool(char *add_type, int add_cost, char *add_name, int add_power); 
 	tool(const tool &to_copy);
 	void display(); //display function
 	~tool(); //destrucor
 
 	protected:
+	char *name;
 	int power;//how much power it has?
 
-
-
-
 };
+
 
 class treasure: public object
 {
 	public:
 	treasure(); //default constructor
 	//constructor with args
-	treasure(char *add_name, int add_cost, char *add_type, bool is_empty); 
+	treasure(char *add_type, int add_cost, char *add_name, bool is_empty); 
 	treasure(const treasure &to_copy); //copy constructor
 	~treasure(); // destructor
 	void display();
 
 	protected:
 	bool is_empty; //will it dissapear when empty?
-//	list *loot_list; //pointer to a linked list of loot?
+	int value; // Treasure chests appear to be defined to only contain whiffles.
 
 };
+
 
 class obstacle: public object
 {
 	public:
 	obstacle(); //default constructor
 	//constructor with args
-	obstacle(char *add_name, int add_cost, char *add_type, int add_b_amount);
+	obstacle(char *add_type, int add_cost, char *add_name, int add_b_amount);
 	obstacle(const obstacle &to_copy); //copy constructor
 	~obstacle(); // destructor
 	void display();
 
 	protected:
 	int b_amount; //how much to break it?
+
 };
 
 #endif
