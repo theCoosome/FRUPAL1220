40255eb6c4dfc186b22ec508f874ed4a38073612
Leah Moser <lmoser@babbage.cs.pdx.edu>
2020-12-01 11:19:46 -0800

    beautified split screen

--- split.cpp
+++ split.cpp
@@ -18,14 +18,16 @@ int mainle(int whiffles, int energy)
 	int y_beg, x_beg;
 	int x_max, y_max;
 	int height, width;
-	height = LINES -1;
+	height = LINES;
 	y_beg = 0;
 	width = COLS/4;
 	x_beg = COLS - width;
 	getmaxyx(stdscr, y_max, x_max);
 	int left, right, top, bottom, tl, tr, bl, br;
 	left = right = 0;
-	top = bottom = tl = tr = bl = br = 95;
+	top = bottom = 32;
+	tl = tr = 42;
+	bl = br = 42;
 	string tmp = to_string(whiffles);
     	char const *to_add = tmp.c_str();
 	string temp = to_string(energy);
