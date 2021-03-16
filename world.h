2a0b3f93dd9e952962f6300f10f0e8ff33a6355b
brmdagoat <baileymanzer@gmail.com>
2021-03-16 14:39:25 -0700

    removed unneccessary files, and modified formatting in world.h

--- world.h
+++ world.h
@@ -4,42 +4,42 @@
 // Stores information about a single Grovnik tile.
 class grovnik {
 	public:
-	grovnik();
-	grovnik(const grovnik & copy); // Copy constructor
-	grovnik(int terr_type, object * data); // complex constructor
+    grovnik();
+    grovnik(const grovnik & copy); // Copy constructor
+    grovnik(int terr_type, object * data); // complex constructor
+    ~grovnik();
 
-	~grovnik();
+	  void setData(int terr_type, object * data);
 
-	void setData(int terr_type, object * data);
-
-	//private: 'proper' structure dictates use of private variables,
-	// but having a manager for the data makes that unwieldly and unnessecary.
-	int terrain; // type of terrain
-	object * poi; // Point of interest on this tile
+    //private: 'proper' structure dictates use of private variables,
+    // but having a manager for the data makes that unwieldly and unnessecary.
+    int terrain; // type of terrain
+    object * poi; // Point of interest on this tile
 };
 
 class World {
 	public:
-	World();
-	World(unsigned int width1, unsigned int length1);
-	// The world should never by directly copied.
+    World();
+    World(unsigned int width1, unsigned int length1);
+    // The world should never by directly copied.
 
-	~World();
+    ~World();
 
-	// grovnik builder for use by world builders
-	void setGrovnik(unsigned int y, unsigned int x, int terr_type, object * data);
-	void clearfog(unsigned int y1, unsigned int x1);
-	void clearfog_rad(unsigned int y1, unsigned int x1, unsigned int radius);
-	char ** fileRead(int * heroy, int * herox);
-	grovnik * getAt(unsigned int y, unsigned int x);
-	bool getfog(unsigned int y1, unsigned int x1);
-	bool clearPOI(int y, int x);
+    // grovnik builder for use by world builders
+    void setGrovnik(unsigned int y, unsigned int x, int terr_type, object * data);
+    void clearfog(unsigned int y1, unsigned int x1);
+    void clearfog_rad(unsigned int y1, unsigned int x1, unsigned int radius);
+    char ** fileRead(int * heroy, int * herox);
+    grovnik * getAt(unsigned int y, unsigned int x);
+    bool getfog(unsigned int y1, unsigned int x1);
+    bool clearPOI(int y, int x);
+
 #ifdef TEST
-	bool ** getfog();
+    bool ** getfog();
 #endif
 
 	private:
-	unsigned int width, height;
-	grovnik ** tiles;
-	bool ** fog; // false is fog, true is clear
+    unsigned int width, height;
+    grovnik ** tiles;
+    bool ** fog; // false is fog, true is clear
 };
